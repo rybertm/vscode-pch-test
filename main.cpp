a06aed3cf9e4c5c3396b34c9145cf633b8ec594d
Robert Broketa <robertbroketa@gmail.com>
2020-06-05 22:30:51 -0300

    initial files

---
+++ main.cpp
@@ -0,0 +1,35 @@
+namespace nm
+{
+    class nmspace
+    {
+    public:
+        nmspace(/* args */) {}
+        ~nmspace() {}
+        static int a;
+    };
+
+    int nmspace::a = 0;
+
+} // namespace nm
+
+class test
+{
+private:
+    nm::nmspace n1;
+    nm::nmspace n2;
+    nm::nmspace n3;
+    nm::nmspace n4;
+    std::vector<nm::nmspace> itens;
+    static int i;
+
+public:
+    test(/* args */) {}
+    ~test() {}
+};
+
+int test::i = nm::nmspace::a;
+
+int main()
+{
+    return 0;
+}
---
+++ pch.h
@@ -0,0 +1,16 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include <algorithm>
+#include <cstdint>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <set>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#endif
